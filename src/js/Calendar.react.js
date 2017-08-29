eb7e9003b6ad8e650d09b3805a4abdcae26d1303
Bill42362 <bill42362@gmail.com>
2017-08-29 14:28:27 +0800

    Done building process.

---
+++ src/js/Calendar.react.js
@@ -0,0 +1,12 @@
+// Calendar.react.js
+'use strict';
+import React from 'react';
+import '../css/calendar.less';
+
+class Calendar extends React.Component {
+    render() {
+        return <div className='calendar'></div>;
+    }
+}
+
+export default Calendar;
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// index.js
+'use strict';
+import Calendar from './Calendar.react.js';
+
+export default { Calendar };
